c54cda32f20f7ceea9871df86276dc39d56d807f
swap3225 <swap3225@gmail.com>
2022-11-07 15:45:05 +0530

    Initial commit

---
+++ 01_hello.py
@@ -0,0 +1,4 @@
+a = input("Enter the temperature in degree celcius that you want to convert in degree farhenheit: ")
+a = int(a)
+frt = (9/5)*a + 32
+print("Your Temperature in degree farhenheit is: ",frt)
